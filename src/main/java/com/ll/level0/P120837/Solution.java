f071a1f6f4cc68a692991a306098bfd8cb30249f
havegrit <shin_jaean@naver.com>
2023-03-03 23:34:27 +0900

    120837 이해하기 쉬운 버전으로 수정

--- src/main/java/com/ll/level0/P120837/Solution.java
+++ src/main/java/com/ll/level0/P120837/Solution.java
@@ -2,6 +2,13 @@ package com.ll.level0.P120837;
 
 public class Solution {
     public int solution(int hp) {
-        return hp/5 + (hp%5)/3 + ((hp%5)%3)/1;
+        int generalAntATK = 5;
+        int soldierAntATK = 3;
+        int workerAntATK = 1;
+        int generalAnt, soldierAnt, workerAnt;
+        generalAnt = hp/generalAntATK;
+        soldierAnt = (hp%generalAntATK)/soldierAntATK;
+        workerAnt = ((hp%generalAntATK)%soldierAntATK)/workerAntATK;
+        return generalAnt + soldierAnt + workerAnt;
     }
 }
