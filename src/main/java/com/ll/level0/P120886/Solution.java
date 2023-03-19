8c47d5d03c602ae9cb78dadc5977e5c8179fd4ef
havegrit <shin_jaean@naver.com>
2023-03-19 21:10:18 +0900

    (120886) TDD GREEN// allpe, apple => 0

--- src/main/java/com/ll/level0/P120886/Solution.java
+++ src/main/java/com/ll/level0/P120886/Solution.java
@@ -1,10 +1,15 @@
 package com.ll.level0.P120886;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(String before, String after) {
         int answer = 0;
         for (String bit : after.split("")) {
-            if(before.contains(bit)) answer++;
+            if(before.contains(bit)) {
+                answer++;
+                before = before.replace(bit, "");
+            }
         }
         return answer == after.length()?1:0;
     }
