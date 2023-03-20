f44853b1373cf0b5861b8bcd98f6388e4f07a2e1
havegrit <shin_jaean@naver.com>
2023-03-20 17:57:47 +0900

    p120848 TDD GREEN(7 => 3)

--- src/main/java/com/ll/level0/P120848/Solution.java
+++ src/main/java/com/ll/level0/P120848/Solution.java
@@ -3,10 +3,13 @@ package com.ll.level0.P120848;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        int factorial = 1;
         for (int i = 10; i > 0; i--) {
+            int factorial = 1;
             for (int j = i; j > 0 ; j--) {
                 factorial *= j;
+                if (factorial > n) {
+                    break;
+                }
             }
             if (factorial <= n) {
                 answer = i;
