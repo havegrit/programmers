5650db729d2b0a7cf815c3b642596f25e2e370c7
havegrit <shin_jaean@naver.com>
2023-03-20 17:36:42 +0900

    p120848 TDD RED (3628800 => 10)

---
+++ src/main/java/com/ll/level0/P120848/Solution.java
@@ -0,0 +1,8 @@
+package com.ll.level0.P120848;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/P120848/SolutionTest.java
@@ -0,0 +1,16 @@
+package com.ll.level0.P120848;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    @DisplayName("3628800 => 10")
+    void t1() {
+        assertThat(new Solution().solution(3628800)).isEqualTo(10);
+    }
+}
