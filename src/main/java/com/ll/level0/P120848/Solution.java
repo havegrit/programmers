dfa2dec7d62fea5742c661aac2983b2c0f6e06c5
havegrit <shin_jaean@naver.com>
2023-03-20 17:48:42 +0900

    p120848 TDD GREEN (3628800 => 10)

--- src/main/java/com/ll/level0/P120848/Solution.java
+++ src/main/java/com/ll/level0/P120848/Solution.java
@@ -3,6 +3,16 @@ package com.ll.level0.P120848;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
+        int factorial = 1;
+        for (int i = 10; i > 0; i--) {
+            for (int j = i; j > 0 ; j--) {
+                factorial *= j;
+            }
+            if (factorial <= n) {
+                answer = i;
+                break;
+            }
+        }
         return answer;
     }
 }
