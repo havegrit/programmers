c9a65f34d22cd84a359464689b55d65766c6cc57
havegrit <shin_jaean@naver.com>
2023-03-30 02:04:10 +0900

    120864 리팩토링

--- src/main/java/com/ll/level0/P120864/Solution.java
+++ src/main/java/com/ll/level0/P120864/Solution.java
@@ -4,22 +4,17 @@ public class Solution {
     public int solution(String my_string) {
         int answer = 0;
         StringBuilder num = new StringBuilder();
-        try {
-            Integer.parseInt(my_string);
-        } catch (NumberFormatException e) {
-            for (int i = 0; i < my_string.length(); i++) {
-                if (Character.isDigit(my_string.charAt(i))) {
-                    num.append(my_string.charAt(i));
-                } else {
-                    if(num.length() > 0) {
-                        answer += Integer.parseInt(num.toString());
-                        num.setLength(0);
-                    }
+        for (int i = 0; i < my_string.length(); i++) {
+            if (Character.isDigit(my_string.charAt(i))) {
+                num.append(my_string.charAt(i));
+            } else {
+                if(num.length() > 0) {
+                    answer += Integer.parseInt(num.toString());
+                    num.setLength(0);
                 }
             }
-            answer+=Integer.parseInt(num.toString());
-            return answer;
         }
-        return Integer.parseInt(my_string);
+        if(num.length() > 0) answer+=Integer.parseInt(num.toString());
+        return answer;
     }
 }
