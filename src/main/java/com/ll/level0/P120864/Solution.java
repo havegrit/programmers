97b7181f86ea087b30edc98f249d2f1d325c2381
havegrit <shin_jaean@naver.com>
2023-03-30 01:55:56 +0900

    120864 반례 찾기

---
+++ src/main/java/com/ll/level0/P120864/Solution.java
@@ -0,0 +1,24 @@
+package com.ll.level0.P120864;
+
+public class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        StringBuilder num = new StringBuilder();
+        try {
+            Integer.parseInt(my_string);
+        } catch (NumberFormatException e) {
+            for (int i = 0; i < my_string.length(); i++) {
+                if (Character.isDigit(my_string.charAt(i))) {
+                    num.append(my_string.charAt(i));
+                } else {
+                    if(num.length() > 0) {
+                        answer += Integer.parseInt(num.toString());
+                        num.setLength(0);
+                    }
+                }
+            }
+            return answer;
+        }
+        return Integer.parseInt(my_string);
+    }
+}
---
+++ src/test/java/com/ll/level0/P120864/SolutionTest.java
@@ -0,0 +1,30 @@
+package com.ll.level0.P120864;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    @DisplayName("aAb1B2cC34oOp => 37")
+    void t1() {
+        assertThat(new Solution().solution("aAb1B2cC34oOp")).isEqualTo(37);
+    }
+    @Test
+    @DisplayName("1a2b3c4d123Z => 133")
+    void t2() {
+        assertThat(new Solution().solution("1a2b3c4d123Z")).isEqualTo(133);
+    }
+    @Test
+    @DisplayName("1234123 => 1234123")
+    void t3() {
+        assertThat(new Solution().solution("1234123")).isEqualTo(1234123);
+    }
+    @Test
+    @DisplayName("abcd => 0")
+    void t4() {
+        assertThat(new Solution().solution("abcd")).isEqualTo(0);
+    }
+}
