2b612c63e368fe72fc75e601e6f277adfc9c27bd
havegrit <shin_jaean@naver.com>
2023-03-31 00:34:28 +0900

    abcabcadc => d & abdc => abdc (GREEN)

--- src/main/java/com/ll/level0/P120896/Solution.java
+++ src/main/java/com/ll/level0/P120896/Solution.java
@@ -1,10 +1,12 @@
 package com.ll.level0.P120896;
 
-import java.util.Arrays;
-
 public class Solution {
     public String solution(String s) {
-        String answer = "";
-        return answer;
+        for (int i = 0; i < s.length()-1; i++) {
+            if (s.substring(i + 1, s.length() - 1).contains(s.subSequence(i,i+1))) {
+                s = s.replaceAll(Character.toString(s.charAt(i)), " ");
+            }
+        }
+        return s.stripIndent();
     }
 }
