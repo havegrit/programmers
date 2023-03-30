5d504236002001e6e585d678bd6b63955b6e44f0
havegrit <shin_jaean@naver.com>
2023-03-31 00:17:32 +0900

    120896 TDD RED

---
+++ src/main/java/com/ll/level0/P120896/Solution.java
@@ -0,0 +1,10 @@
+package com.ll.level0.P120896;
+
+import java.util.Arrays;
+
+public class Solution {
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ src/test/java/com/ll/level0/P120896/SolutionTest.java
@@ -0,0 +1,25 @@
+package com.ll.level0.P120896;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SolutionTest {
+
+    @Test
+    @DisplayName("abcabcadc => d")
+    void t1() {
+        assertThat(new Solution().solution("abcabcadc")).isEqualTo("d");
+    }
+    @Test
+    @DisplayName("abdc => abdc")
+    void t2() {
+        assertThat(new Solution().solution("abdc")).isEqualTo("abdc");
+    }
+    @Test
+    @DisplayName("hello => eho")
+    void t3() {
+        assertThat(new Solution().solution("hello")).isEqualTo("eho");
+    }
+}
