66df32a3ce15d02e6c61bff5e47336a51fcaf60f
havegrit <shin_jaean@naver.com>
2023-03-31 00:50:17 +0900

    BLUE

--- src/main/java/com/ll/level0/P120896/Solution.java
+++ src/main/java/com/ll/level0/P120896/Solution.java
@@ -6,7 +6,10 @@ import java.util.stream.Collectors;
 public class Solution {
     public String solution(String s) {
         for (int i = 0; i < s.length()-1; i++) {
-            if (s.substring(i + 1, s.length() - 1).contains(s.subSequence(i,i+1))) {
+            System.out.println(s.substring(i + 1, s.length()));
+            System.out.println(s.subSequence(i, i + 1));
+            if (s.substring(i + 1, s.length()).contains(s.subSequence(i,i+1))) {
+
                 s = s.replaceAll(Character.toString(s.charAt(i)), " ");
             }
         }
